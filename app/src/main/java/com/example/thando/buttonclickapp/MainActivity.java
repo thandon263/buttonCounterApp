1f29a60993d1121485e0f8ff18020ddba2de07e4
Thando <ncubethando20@gmail.com>
2018-08-06 23:21:47 -0400

    Completed - implementing the onSave for the instance of bundle.

--- app/src/main/java/com/example/thando/buttonclickapp/MainActivity.java
+++ app/src/main/java/com/example/thando/buttonclickapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.thando.buttonclickapp;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,13 +13,16 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final String TAG = "MainActivity";
     // Variables to reference the objects
     // We used to create the layout.
     private EditText userInput;
     private TextView textView;
+    private final String TEXT_CONTENTS = "TextContents";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.button_click_activity);
 
@@ -42,5 +47,61 @@ public class MainActivity extends AppCompatActivity {
         // This is to initialize our onClickLister to be attached to the OnClick listener.
         // This will handle the event when it happens.
         button.setOnClickListener(ourOnClickListener);
+        Log.d(TAG, "onCreate: out");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        // Get the saved instance from the bundle and restore it into the app
+        // That is why this method is called after the (super method).
+        textView.setText(savedInstanceState.getString(TEXT_CONTENTS));
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, " onSaveInstanceState: in");
+        // This will store the instance of the application state
+        // into the bundle. before the (super method is called).
+        outState.putString(TEXT_CONTENTS, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, " onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
     }
 }
